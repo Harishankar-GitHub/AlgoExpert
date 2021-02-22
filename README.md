f0192d804859e30ca7eab56db575344143b57008
HARISHANKAR BHAT R <rhsb1997@gmail.com>
2021-02-22 20:19:54 +0530

    2. Validate Subsequence

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # :sparkles: [AlgoExpert](https://www.algoexpert.io/product) :sparkles:
 
-| No. | Question | Category | Difficulty
-|--|--|--|--|
-| 1. | [Two Number Sum](https://www.algoexpert.io/questions/Two%20Number%20Sum) | Arrays | Easy |
+| No. | Question | Category | Difficulty | Solution
+|--|--|--|--|--|
+| 1. | [Two Number Sum](https://www.algoexpert.io/questions/Two%20Number%20Sum) | Arrays | Easy | [Click here](https://github.com/Harishankar-GitHub/AlgoExpert/blob/main/Two%20Number%20Sum/src/Program.java) |
+| 2. | [Validate Subsequence](https://www.algoexpert.io/questions/Validate%20Subsequence) | Arrays | Easy | [Click here]() |
---
+++ Validate Subsequence/src/Program.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Program
+{
+    public static void main(String[] args)
+    {
+        List<Integer> integerList = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = List.of(1, 6, -1, 10);
+
+        System.out.println(isValidSubsequence_using_ForLoop(integerList,sequence));
+        System.out.println(isValidSubsequence_using_WhileLoop(integerList,sequence));
+    }
+
+    public static boolean isValidSubsequence_using_ForLoop(List<Integer> array, List<Integer> sequence)
+    {
+        // O(n) time | O(1) space - where n is the length of the array
+
+        int index = 0;
+
+        for (int num : array)
+        {
+            if (num == sequence.get(index))
+            {
+                if (index == sequence.size() - 1) return true;
+                index++;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isValidSubsequence_using_WhileLoop(List<Integer> array, List<Integer> sequence)
+    {
+        // O(n) time | O(1) space - where n is the length of the array
+
+        int arrIndex = 0;
+        int seqIndex = 0;
+
+        while (arrIndex < array.size() && seqIndex < sequence.size())
+        {
+            if (array.get(arrIndex) == sequence.get(seqIndex))
+            {
+                seqIndex++;
+            }
+            arrIndex++;
+        }
+        return seqIndex == sequence.size();
+    }
+}
