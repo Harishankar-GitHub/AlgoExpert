0690b607f80493702b0374a192fc1d2da9ad5852
HARISHANKAR BHAT R <rhsb1997@gmail.com>
2021-02-24 21:01:15 +0530

    4. Non-Constructible Change

---
+++ Non-Constructible Change/src/Program.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Program
+{
+    public static void main(String[] args)
+    {
+        int[] coins = {5, 7, 1, 1, 2, 3, 22};
+        System.out.println(nonConstructibleChange(coins));
+    }
+
+    public static int nonConstructibleChange(int[] coins)
+    {
+        // O(nlogn + n) = O(nlogn) time | O(1) space
+        // nlogn is for sorting the array
+        // n is to iterate the array once after sorting
+
+        Arrays.sort(coins);
+
+        int currentChangeCreated = 0;
+
+        for (int coin : coins)
+        {
+            if (coin > currentChangeCreated + 1)
+            {
+                return currentChangeCreated + 1;
+            }
+            currentChangeCreated += coin;
+        }
+        return currentChangeCreated + 1;
+    }
+}
